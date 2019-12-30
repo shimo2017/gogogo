f1e88735dabb0f6d31f6d0024ce1d972ac28566a
shimo <ippon1983@gmail.com>
2019-12-31 00:56:25 +0900

    lesson 関数

--- hellow.go
+++ hellow.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func add(x int, y int) (int, int) {
+	return x + y, x - y
+}
+
+func cal(price, item int) (result int) {
+	result = price * item
+	return result
+}
+
+func convert(price int) float64 {
+	return float64(price)
+}
 
 func main() {
-	fmt.Println("Hellow GO!!")
+	r1, r2 := add(10, 20)
+	fmt.Println(r1, r2)
+
+	r3 := cal(100, 2)
+	fmt.Println(r3)
+
+	r4 := convert(100)
+	fmt.Println(r4)
+
+	f := func(x int) {
+		fmt.Println("inner func", x)
+	}
+	f(1)
+
+	func(x int) {
+		fmt.Println("inner func", x)
+	}(2)
 }
