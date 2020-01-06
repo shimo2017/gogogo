43a59044e296eeb6906545a2edea6412f213752f
shimo <ippon1983@gmail.com>
2020-01-07 00:38:38 +0900

    lesson2 クロシージャ

--- lesson2/lesson2.go
+++ lesson2/lesson2.go
@@ -10,10 +10,21 @@ func incrementGenerator() func() int {
 	}
 }
 
+func circleArea(pi float64) func(redius float64) float64 {
+	return func(redius float64) float64 {
+		return pi * redius * redius
+	}
+}
+
 func main() {
 	cunter := incrementGenerator()
 	fmt.Println(cunter())
 	fmt.Println(cunter())
 	fmt.Println(cunter())
 
+	c1 := circleArea(3.14)
+	fmt.Println(c1(2))
+
+	c2 := circleArea(3)
+	fmt.Println(c2(2))
 }
