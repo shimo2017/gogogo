46b77fd530412ca0409cb5fdd6fc9bb6bd2587a2
shimo <ippon1983@gmail.com>
2020-01-08 00:00:51 +0900

    制御構文if

---
+++ lesson4/lesson4.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func by2(num int) string {
+	if num%2 == 0 {
+		return "ok"
+	} else {
+		return "no"
+	}
+}
+
+func main() {
+	/*	num := 6
+		if num%2 == 0 {
+			fmt.Println("by 2")
+		} else if num%3 == 0 {
+			fmt.Println("by 3")
+		} else {
+			fmt.Println("else")
+		}
+
+		x, y := 10, 10
+		if x == 10 && y == 10 {
+			fmt.Println("&&")
+		}
+
+		if x == 10 || y == 10 {
+			fmt.Println("||")
+		}
+	*/
+
+	result := by2(10)
+	if result == "ok" {
+		fmt.Println("great")
+	}
+
+	if result2 := by2(10); result2 == "ok" {
+		fmt.Println("great2")
+	}
+}
