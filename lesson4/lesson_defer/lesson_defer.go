7be17a377cd6658eb539d0ee2091e34c36d7bdae
shimo <ippon1983@gmail.com>
2020-01-13 01:16:01 +0900

    lesson defer og

---
+++ lesson4/lesson_defer/lesson_defer.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func foo() {
+	defer fmt.Println("world foo")
+	fmt.Println("hello foo")
+}
+
+func main() {
+	foo()
+
+	defer fmt.Println("world")
+	fmt.Println("hello")
+
+	fmt.Println("-----------------")
+
+	fmt.Println("run")
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	defer fmt.Println(3)
+	fmt.Println("success")
+
+	fmt.Println("--------------")
+
+	file, _ := os.Open("./lesson_defer.go")
+	defer file.Close()
+	data := make([]byte, 100)
+	file.Read(data)
+	fmt.Println(string(data))
+}
